7e64896d6584d44fcb29a1bf818f1aa195d19aa2
smiles2408 <87461037+smiles2408@users.noreply.github.com>
2023-08-03 01:04:40 -0400

    Second commit

---
+++ BlazorAppToDoList/Data/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BlazorAppToDoList.Data.Models
+{
+	public class TaskModel
+	{
+		public TaskModel()
+		{
+		}
+		public string TaskName { get; set; }
+		public bool IsComplete { get; set; }
+
+	}
+}
+
